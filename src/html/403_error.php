0d07fb1365cd6bb3a575978f0733e8a532ac762b
Peeter Marvet <pets@tehnokratt.net>
2024-08-31 11:54:30 +0300

    html syntax fixes
    unclosed tags, </br>, moving inline style from tag attribute to <style> etc

--- src/html/403_error.php
+++ src/html/403_error.php
@@ -1,75 +1,73 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-  <title>[Error 403] Unexpected action detected!</title>
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Oxanium:wght@400&display=swap" rel="stylesheet">
-  <style>
-    * {
-      font-family: 'Oxanium', cursive;
-      box-sizing: border-box;
-    }
+    <title>[Error 403] Unexpected action detected!</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Oxanium:wght@400&display=swap" rel="stylesheet">
+    <style>
+        * {
+            font-family: 'Oxanium', cursive;
+            box-sizing: border-box;
+        }
 
-    body {
-      background-color: #f2f8ff;
-      color: #333;
-      text-align: center;
-      padding: 50px;
-    }
+        body {
+            background-color: #f2f8ff;
+            color: #333;
+            text-align: center;
+            padding: 50px;
+        }
 
-    #app {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      height: 100vh;
-    }
+        #app {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
 
-    #app div:first-child {
-      font-size: 128px;
-      font-weight: bold;
-      margin-bottom: 20px;
-      color: #2196f3;
-    }
+        #app div:first-child {
+            font-size: 128px;
+            font-weight: bold;
+            margin-bottom: 20px;
+            color: #2196f3;
+        }
 
-    #app .txt {
-      font-size: 50px;
-      line-height: 1.5;
-      color: #333;
-    }
+        #app .txt {
+            font-size: 50px;
+            line-height: 1.5;
+            color: #333;
+        }
 
-    #app .txt span {
-      font-size: 1.8rem;
-      color: #666;
-    }
+        #app .txt span {
+            font-size: 1.8rem;
+            color: #666;
+        }
 
-    #app .txt b {
-      font-family: 'Oxanium', sans-serif;
-    }
+        #app .txt strong {
+            font-family: 'Courier New', Courier, monospace;
+        }
 
-    #app .txt .blink {
-      animation: blink 1s step-end infinite;
-    }
-
-    @keyframes blink {
-      50% {
-        border-color: transparent;
-      }
-    }
-  </style>
+        @keyframes blink {
+            50% {
+                border-color: transparent;
+            }
+        }
+    </style>
 </head>
 
 <body>
-  <div id="app">
+<div id="app">
     <div>403</div>
     <div class="txt">
-      Unexpected action detected!<br>
-      <span>Please contact our help desk with request id:</br>
-        <b style="font-family: 'Courier New', Courier, monospace;">
+        Unexpected action detected!<br>
+        <span>Please contact our help desk with request id:<br>
+        <strong>
           <?= $_SERVER['REQUEST_ID'] ?>
+        </strong>
+      </span>
     </div>
-  </div>
+</div>
 </body>
 
 </html>
